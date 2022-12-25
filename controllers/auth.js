fbc3a0d29d330195c59f0d2dd9629be991f6e1b6
emamaria <emascript@gmail.com>
2022-12-25 17:11:57 +0100

    register user to database ok

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,46 @@
 const { response } = require('express') 
+const User = require('../models/User')
 
  
- const registerUser = (req, res = response) => {
+ const registerUser = async(req, res = response) => {
 
     console.log(req.body)
 
     const {email, name, password} = req.body
 
-    console.log(email, name, password)
+     console.log(email, name, password)
 
-    return res.json({
-        ok: true,
-        msg: 'user register'
-    })
+     try {
+
+        const user = await User.findOne({email: email})
+
+        if(user){
+            return res.status(400).json({
+                ok: false,
+                msg: 'the user already exists'
+            })
+        }
+
+
+       const userDB = new User(req.body)
+
+      await userDB.save()
+
+      return res.status(201).json({
+        ok:true,
+        id: userDB.id,
+        name: name,
+        email: email
+      })
+        
+     } catch (error) {
+        return res.json({
+            ok: false,
+            msg: 'call the admin'
+        })
+     }
+
+   
 }
 
 
