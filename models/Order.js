6c510ab7416c071e43648149c29f3f560e1e7427
emamaria <emascript@gmail.com>
2022-12-31 14:02:00 +0100

    ok orders model

---
+++ models/Order.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require("mongoose");
+
+const OrderSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    userId: { 
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    orderItems: {
+        type: Array,
+        required: true,
+        trim: true 
+    },
+    shippingAddress: {
+        type: Object,
+        required: true, 
+        trim: true
+    },
+    orderPrice: {
+        type: Number,
+        required: true, 
+        trim: true
+    },
+    orderDelivered: {
+         type: Boolean,
+         required: true,
+         trim: true,
+        },
+    transactionId: {
+        type: String,
+        required: true,
+        trim: true}, 
+    
+},
+{
+    timestamps: true,
+})
+
+
+module.exports = model('Order', UserSchema)
