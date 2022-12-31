926d378d393c99398d6628bf721b41e6b932351f
emamaria <emascript@gmail.com>
2022-12-31 18:04:46 +0100

    ok payment router and controller

---
+++ routes/payment.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const {checkout} = require('../controllers/payment');
+const { validateJWT } = require('../middlewares/jwt-validation');
+
+
+const router = Router();
+
+
+router.post("/", validateJWT, checkout)
+
+
+module.exports = router;
