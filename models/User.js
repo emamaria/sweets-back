110fa1a70928c2c32a4605429a7f4c757eb617f8
emamaria <emascript@gmail.com>
2023-01-07 12:36:29 +0100

    trim and timestamp in user model

--- models/User.js
+++ models/User.js
@@ -3,18 +3,25 @@ const { Schema, model } = require("mongoose");
 const UserSchema = Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        trim: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        trim: true
     },
     password: {
         type: String,
-        required: true
-    }
+        required: true,
+        trim: true
+    },
+    
 
+},
+{
+    timestamps: true,
 })
 
 
