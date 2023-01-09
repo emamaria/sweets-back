ca1ee684b7422f17d96a4c93e4752cefb997aa25
emamaria <emascript@gmail.com>
2023-01-09 16:17:16 +0100

    correct error message login register

--- routes/auth.js
+++ routes/auth.js
@@ -10,13 +10,13 @@ const router = Router();
 router.post('/register',[
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is required with correct format').isEmail(),
-    check('password', 'Password required minimun 8 characters, 1 number, 1 speacial character, 1 uppercase, 1 lowercase').isStrongPassword(),
+    check('password', 'Password requires at least 8 characters, 1 number, 1 special character, 1 uppercase and 1 lowercase').isStrongPassword(),
 ] , validateFields, registerUser)
 
 
 router.post('/login',[
     check('email', 'Email is required with correct format').isEmail(),
-    check('password', 'Password required minimun 8 characters, 1 number, 1 speacial character, 1 uppercase, 1 lowercase').isStrongPassword(),
+    check('password', 'Password requires at least 8 characters, 1 number, 1 special character, 1 uppercase and 1 lowercase').isStrongPassword(),
 ], validateFields ,loginUser) 
 
 //mando el token por el header y contrasto la firma
